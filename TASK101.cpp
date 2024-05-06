9b33b7885ce84269ef3997bf0090e9e6b2884c43
msiyem <siyem2574@gmail.com>
2024-05-06 14:42:02 +0600

    update

--- TASK101.cpp
+++ TASK101.cpp
@@ -1,45 +1,77 @@
 #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <iostream>
 
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 400
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 700
+#define R 150
+SDL_Window *win = NULL;
+SDL_Renderer *rend = NULL;
+bool gameIsRunning=false;
 
-int main(int argc, char* args[]) {
-    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("Circle drawing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+int initializing(){
+if(SDL_Init(SDL_INIT_VIDEO)!=0){
+std::cout<<"Error: SDL failed to initialize\n"<<"SDL Error:"<<" "<<SDL_GetError()<<'\n';
+return 0;
+}
 
-    int centerX = 300;
-    int centerY = 200;
-    int radius = 100;
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red color
-    for (int w = 0; w < radius * 2; w++) {
-        for (int h = 0; h < radius * 2; h++) {
-            int dx = radius - w;
-            int dy = radius - h;
-            if ((dx*dx + dy*dy) <= (radius * radius)) {
-                SDL_RenderDrawPoint(renderer, centerX + dx, centerY + dy);
+ win = SDL_CreateWindow("Drawing Circle",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH,SCREEN_HEIGHT, 0);
+if (!win)
+    {
+       std::cout<<"Error: SDL failed to open window\n"<<"SDL Error:"<<" "<<SDL_GetError()<<'\n';
+       return 0;;
+    }
+
+
+
+    rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    if (!rend)
+    {
+        std::cout<<"Error: SDL failed to create renderer\n"<<"SDL Error:"<<" "<<SDL_GetError()<<'\n';
+        return 0;;
+    }
+
+	return 1;
+}
+
+void  event_loop()
+{
+  SDL_Event event;
+
+  while(SDL_PollEvent(&event))
+     {
+       if(event.type==SDL_QUIT) {gameIsRunning=false; break;}
+     }
+
+}
+
+
+void draw_Circle( int centerX, int centerY, int radius) {
+    for (int x = -radius; x <= radius; x++) {
+        for (int y = -radius; y <= radius; y++) {
+            if (x*x + y*y <= radius*radius) {
+                SDL_RenderDrawPoint(rend, centerX + x, centerY + y);
             }
         }
     }
+}
 
-    SDL_RenderPresent(renderer);
+int main(int argc,char *argv[])
+{
+gameIsRunning=initializing();
 
-    bool quit = false;
-    SDL_Event event;
-
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
+while(gameIsRunning)
+{
+	event_loop();
+    SDL_SetRenderDrawColor(rend, 50, 255, 200, 50);
+	SDL_RenderClear(rend);
+	SDL_SetRenderDrawColor(rend, 250, 0, 0, 0);
+    draw_Circle(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,R);
+	SDL_RenderPresent(rend);
+}
+SDL_DestroyWindow(win);
+SDL_Quit();
+return 0;
 }
